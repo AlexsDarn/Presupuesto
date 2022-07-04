bb074fcba7ed9e68de5f95f466e6e86a3fcd2dff
Alexis Salazar <91296029+AlexsDarn@users.noreply.github.com>
2022-07-03 21:58:29 -0500

    Started the html views and created the styles, also bootstrap, font awesome and google font will be used.

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/add', (_req, _res) => {
+    _res.render('balance/add');
+});
+
+router.post('/add', (_req, _res) => {
+    _res.send('received');
+});
+
 module.exports = router;
