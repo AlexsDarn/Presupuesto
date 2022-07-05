06811b4ae512f19d36cf5a18404d477690fab8cb
Alexis Salazar <91296029+AlexsDarn@users.noreply.github.com>
2022-07-04 21:49:10 -0500

    Insert data on db and small interface.

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,7 +7,12 @@ router.get('/add', (_req, _res) => {
     _res.render('balance/add');
 });
 
-router.post('/add', (_req, _res) => {
+router.post('/add', async (_req, _res) => {
+    const {concept, amount, date, type, category} = _req.body;
+    const newLink = {
+        concept, amount, date, type, category
+    };
+    await pool.query('INSERT INTO balance set ?', [newLink]);
     _res.send('received');
 });
 
